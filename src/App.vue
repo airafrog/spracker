fc157644552c3045aeefe7641d0e62d96eb206da
colmeye <colmeyer1@gmail.com>
2025-07-09 15:46:42 -0500

    two cubez

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,19 @@
 <script setup lang="ts">
-import * as THREE from "three";
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
-}
+import OriginalScene from "./components/OriginalScene.vue";
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <original-scene />
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/PerspectiveView.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import * as THREE from "three";
+import { onMounted, onUnmounted, ref } from "vue";
+
+const props = defineProps<{
+  scene: THREE.Scene;
+}>();
+
+const canvas = ref<HTMLCanvasElement>();
+const canvasContainer = ref<HTMLDivElement>();
+let resizeFunction = () => {};
+
+onMounted(() => {
+  if (!canvas.value) throw new Error("Canvas is not defined");
+  if (!canvasContainer.value) {
+    throw new Error("Canvas container is not defined");
+  }
+
+  const renderer = new THREE.WebGLRenderer({ canvas: canvas.value });
+  renderer.setSize(
+    canvasContainer.value.clientWidth,
+    canvasContainer.value.clientHeight
+  );
+  renderer.setAnimationLoop(animate);
+
+  // ortho camera based on the canvas size
+  const aspect =
+    canvasContainer.value.clientWidth / canvasContainer.value.clientHeight;
+  const camera = new THREE.PerspectiveCamera(
+    75, // fov
+    aspect, // aspect
+    0.1, // near
+    1000 // far
+  );
+  camera.position.z = 5;
+
+  function animate() {
+    renderer.render(props.scene, camera);
+  }
+
+  resizeFunction = () => {
+    if (!canvasContainer.value) return;
+    const width = canvasContainer.value.clientWidth;
+    const height = canvasContainer.value.clientHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+  };
+  window.addEventListener("resize", resizeFunction);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("resize", resizeFunction);
+});
+</script>
+
+<template>
+  <div ref="canvasContainer" class="canvas-container">
+    <canvas ref="canvas" />
+  </div>
+</template>
+
+<style scoped>
+.canvas-container {
+  width: 100%;
+  height: 100%;
+}
+
+canvas {
+  position: absolute;
+}
+</style>
