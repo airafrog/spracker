0ccdb2207b5c110935fa3de4fbb1ecee4155017c
colmeye <colmeyer1@gmail.com>
2025-07-22 18:59:55 -0500

    background color

--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -3,7 +3,7 @@ import { ref } from "vue";
 
 export const useSettingsStore = defineStore("settings", () => {
   const originalSceneBackgroundHex = ref("#120d12");
-  const stackedSceneBackgroundHex = ref("#0b080b");
+  const stackedSceneBackgroundHex = ref("#1a131a");
   const pngBackgroundHex = ref("#3e3546");
 
   return {
